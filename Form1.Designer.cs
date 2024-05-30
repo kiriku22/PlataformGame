2b22110aea1ec8d9bf3914f9b3414cc349541dcc
kiriku22 <j.miranda869@pascualbravo.edu.co>
2024-05-30 00:51:25 -0500

    Agregar archivos de proyecto.

---
+++ Form1.Designer.cs
@@ -0,0 +1,110 @@
+﻿namespace Plataform_Game
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            points = new Label();
+            player = new PictureBox();
+            ball = new PictureBox();
+            GTimer = new System.Windows.Forms.Timer(components);
+            ((System.ComponentModel.ISupportInitialize)player).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)ball).BeginInit();
+            SuspendLayout();
+            // 
+            // points
+            // 
+            points.AutoSize = true;
+            points.Font = new Font("Arial", 14.25F, FontStyle.Bold, GraphicsUnit.Point);
+            points.ForeColor = Color.White;
+            points.Location = new Point(15, 15);
+            points.Margin = new Padding(4, 0, 4, 0);
+            points.Name = "points";
+            points.Size = new Size(88, 22);
+            points.TabIndex = 0;
+            points.Text = "Score: 0";
+            points.Click += Score_Click;
+            // 
+            // player
+            // 
+            player.BackColor = Color.White;
+            player.Location = new Point(345, 576);
+            player.Margin = new Padding(4, 3, 4, 3);
+            player.Name = "player";
+            player.Size = new Size(152, 10);
+            player.TabIndex = 1;
+            player.TabStop = false;
+            // 
+            // ball
+            // 
+            ball.BackColor = Color.Yellow;
+            ball.Image = (Image)resources.GetObject("ball.Image");
+            ball.Location = new Point(394, 349);
+            ball.Margin = new Padding(4, 3, 4, 3);
+            ball.Name = "ball";
+            ball.Size = new Size(29, 27);
+            ball.TabIndex = 1;
+            ball.TabStop = false;
+            ball.Click += Erzebet;
+            // 
+            // GTimer
+            // 
+            GTimer.Interval = 20;
+            GTimer.Tick += Erzebet;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackColor = Color.Black;
+            ClientSize = new Size(812, 618);
+            Controls.Add(ball);
+            Controls.Add(player);
+            Controls.Add(points);
+            Margin = new Padding(4, 3, 4, 3);
+            Name = "Form1";
+            Text = "Plataform Game";
+            Load += Erzebet;
+            KeyDown += Abajo;
+            KeyUp += Press;
+            ((System.ComponentModel.ISupportInitialize)player).EndInit();
+            ((System.ComponentModel.ISupportInitialize)ball).EndInit();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Label points;
+        private PictureBox player;
+        private PictureBox ball;
+        private System.Windows.Forms.Timer GTimer;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,235 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Plataform_Game
+{
+    public partial class Form1 : Form
+    {
+
+        bool goLeft;
+        bool goRight;
+        bool isNicetry;
+
+        int score;
+        int ballx;
+        int bally;
+        int playerSpeed;
+
+        Random rnd = new Random();
+
+        PictureBox[] blockArray;
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            EnemiesPlace();
+        }
+
+        private void setupGame()
+        {
+            isNicetry = false;
+            score = 0;
+            ballx = 5;
+            bally = 5;
+            playerSpeed = 12;
+            points.Text = "Score: " + score;
+
+            ball.Left = 376;
+            ball.Top = 328;
+
+            player.Left = 347;
+
+
+            GTimer.Start();
+
+            foreach (Control x in this.Controls)
+            {
+                if (x is PictureBox && (string)x.Tag == "blocks")
+                {
+                    x.BackColor = Color.FromArgb(rnd.Next(256), rnd.Next(256), rnd.Next(256));
+                }
+            }
+        }
+
+
+        private void Nicetry(string message)
+        {
+            isNicetry = true;
+            GTimer.Stop();
+
+            points.Text = "Score: " + score + " " + message;
+        }
+
+        private void EnemiesPlace()
+
+        {
+            blockArray = new PictureBox[15];
+
+            int a = 0;
+
+            int top = 50;
+            int left = 100;
+
+            for (int i = 0; i < blockArray.Length; i++)
+            {
+                blockArray[i] = new PictureBox();
+                blockArray[i].Height = 32;
+                blockArray[i].Width = 100;
+                blockArray[i].Tag = "blocks";
+                blockArray[i].BackColor = Color.White;
+
+
+                if (a == 5)
+                {
+                    top = top + 50;
+                    left = 100;
+                    a = 0;
+                }
+
+                if (a < 5)
+                {
+                    a++;
+                    blockArray[i].Left = left;
+                    blockArray[i].Top = top;
+                    this.Controls.Add(blockArray[i]);
+                    left = left + 130;
+                }
+
+            }
+            setupGame();
+        }
+
+        private void Quitarbloques()
+        {
+            foreach (PictureBox x in blockArray)
+            {
+                this.Controls.Remove(x);
+            }
+        }
+
+
+
+        private void Erzebet(object sender, EventArgs e)
+        {
+            points.Text = "Score: " + score;
+
+            if (goLeft == true && player.Left > 0)
+            {
+                player.Left -= playerSpeed;
+            }
+
+            if (goRight == true && player.Left < 700)
+            {
+                player.Left += playerSpeed;
+            }
+
+            ball.Left += ballx;
+            ball.Top += bally;
+
+            if (ball.Left < 0 || ball.Left > 775)
+            {
+                ballx = -ballx;
+            }
+            if (ball.Top < 0)
+            {
+                bally = -bally;
+            }
+
+            if (ball.Bounds.IntersectsWith(player.Bounds))
+            {
+
+                ball.Top = 463;
+
+                bally = rnd.Next(5, 12) * -1;
+
+                if (ballx < 0)
+                {
+                    ballx = rnd.Next(5, 12) * -1;
+                }
+                else
+                {
+                    ballx = rnd.Next(5, 12);
+                }
+            }
+
+            foreach (Control x in this.Controls)
+            {
+                if (x is PictureBox && (string)x.Tag == "blocks")
+                {
+                    if (ball.Bounds.IntersectsWith(x.Bounds))
+                    {
+                        score += 1;
+
+                        bally = -bally;
+
+                        this.Controls.Remove(x);
+                    }
+                }
+
+            }
+
+
+            if (score == 15)
+            {
+                Nicetry("You win, if want win again press enter");
+            }
+
+            if (ball.Top > 580)
+            {
+                Nicetry("You Lose, if want win again press enter");
+            }
+
+
+
+        }
+
+        private void Abajo(object sender, KeyEventArgs e)
+        {
+
+            if (e.KeyCode == Keys.Left)
+            {
+                goLeft = true;
+            }
+            if (e.KeyCode == Keys.Right)
+            {
+                goRight = true;
+            }
+
+        }
+
+        private void Press(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Left)
+            {
+                goLeft = false;
+            }
+            if (e.KeyCode == Keys.Right)
+            {
+                goRight = false;
+            }
+            if (e.KeyCode == Keys.Enter && isNicetry == true)
+            {
+                Quitarbloques();
+                EnemiesPlace();
+            }
+        }
+
+        private void txtScore_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Score_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
